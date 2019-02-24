b364198de5c87802a20db2fe23b5605127bb2a25
Nick Weiner <bigweiner@ucdavis.edu>
2019-02-24 08:59:28 -0600

    Fixed all the debugging problems with a working pipe of data! Even accepts the k paramater.

--- Tech Challenge/bed_interval.py
+++ Tech Challenge/bed_interval.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-from collections import defaultdict
+from collections import Counter
 import json
+from numpy import ones
 
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.coverage = defaultdict(int)
+        self.coverage = Counter()
 
     def add_coverage(self, start, end, depth):
         for i in range(start, end + 1):
             if self.start <= i <= self.end:
-                self.coverage[i] += depth
+                self.coverage.update(list(map(int, ones(depth)*i)))
+
+    # @property
+    # def mean_coverage(self):
+    #     running_sum = 0
+    #     for i in range(self.start, self.end + 1):
+    #         running_sum += self.coverage[i]
+    #     return float(running_sum) / self.size
 
     @property
-    def mean_coverage(self):
-        running_sum = 0
-        for i in xrange(self.start, self.end + 1):
-            running_sum += self.coverage[i]
-        return float(running_sum) / self.size
+    def max_coverage(self):
+        if len(self.coverage) == 0:
+            return 0
+        out = self.coverage.most_common(1)[0][1]
+        return out
 
     @property
     def size(self):
@@ -32,33 +40,38 @@ class BedData(object):
         self.reads = {}
 
     def sort_intervals(self):
-        for chr in self.reads:
-            self.reads[chr] = sorted(self.reads[chr], key=lambda x: x.start)
+        for chrm in self.reads:
+            self.reads[chrm] = sorted(self.reads[chrm], key=lambda x: x.start)
 
     def load_bed(self, bed):
         with open(bed) as bf:
             for read in bf:
                 data = read.split()
-                chr = data[0]
-                chr = chr.replace('chr', '')  # get value down to only number or letter
+                chrm = data[0]
+                chrm = chrm.replace('chr', '')  # get value down to only number or letter
                 start_pos = int(data[1])
                 end_pos = int(data[2])
-                if chr not in self.reads:
-                    self.reads[chr] = []
-                self.reads[chr].append(Interval(start_pos, end_pos))
+                if chrm not in self.reads:
+                    self.reads[chrm] = []
+                self.reads[chrm].append(Interval(start_pos, end_pos))
         self.sort_intervals()
-        self.chr_totals = self.get_total_bases_each_contig()
+        self.chr_totals = self.get_total_bases_from_contig()
 
-    def add_interval(self, chr, start, end):
-        if not chr in self.reads:
-            self.reads[chr] = []
-        self.reads[chr].append(Interval(start, end))
+    def add_interval(self, chrm, start, end):
+        if chrm not in self.reads:
+            self.reads[chrm] = []
+        self.reads[chrm].append(Interval(start, end))
 
-    def is_overlap(self, chr, start_pos, end_pos, padding=0):
+    def copy_interval(self, chrm, interval):
+        if chrm not in self.reads:
+            self.reads[chrm] = []
+        self.reads[chrm].append(interval) #ensures coverage values being shared
+
+    def is_overlap(self, chrm, start_pos, end_pos, padding=0):
         start_pos -= padding
         end_pos += padding
-        chr = chr.replace('chr', '') # get value down to only number or letter
-        if chr not in self.reads:
+        chrm = chrm.replace('chr', '') # get value down to only number or letter
+        if chrm not in self.reads:
             return False
         chr_data = self.reads[chr]
         can_stop = False
@@ -76,44 +89,50 @@ class BedData(object):
                     break
                 if coord.start > end_pos:
                     break
-                if found_overlap:
-                    self.
             can_stop = True
         return found_overlap
 
     def get_total_bases_from_contig(self):
         chr_totals = {}
-        for chr in self.reads:
+        for chrm in self.reads:
             total_bases = 0
-            for coord in self.reads[chr]:
+            for coord in self.reads[chrm]:
                 total_bases += coord.size
-            chr_totals[chr] = total_bases
+            chr_totals[chrm] = total_bases
         return chr_totals
 
 
-    def get_self_overlaps(self, set_overlap):
-        overs = BedData()
-        for chr in self.reads:
-            for interval in self.reads[chr]:
-                padding = 0
-                if set_overlap<0:
-                    padding = abs(set_overlap)
-                if self.is_overlap(chr, interval.start, interval.end, padding):
+    # def get_self_overlaps(self, set_overlap):
+    #     overs = BedData()
+    #     for chr in self.reads:
+    #         for interval in self.reads[chr]:
+    #             padding = 0
+    #             if set_overlap<0:
+    #                 padding = abs(set_overlap)
+    #             if self.is_overlap(chr, interval.start, interval.end, padding):
 
 
-    def dump_intervals(self, file_name):
+    def dump_intervals(self, file_name, k=1):
         self.sort_intervals()
-        with open(file_name, 'w') as of:
-            for chr in self.reads:
-                for interval in self.reads[chr]:
-                    of.write('{0}\t{1}\t{2}\n'.format(chr, interval.start, interval.end))
+        with open(file_name, 'w') as output_f:
+            for chrm in self.reads:
+                for interval in self.reads[chrm]:
+                    if interval.max_coverage>=k:
+                        line = '{0}\t{1}\t{2}\t{3}\n'.format(chrm, interval.start,
+                                                                interval.end,
+                                                                # interval.mean_coverage,
+                                                                interval.max_coverage)
+                        output_f.write(line)
 
-    def dump_intervals_json(self, file_name):
+    def dump_intervals_json(self, file_name, k=1):
         self.sort_intervals()
         dump_data = {}
-        for chr in self.reads:
-            dump_data[chr] = []
-            for interval in self.reads[chr]:
-                dump_data[chr].append({'start': interval.start, 'size': interval.size})
-        with open(file_name, 'w') as of:
-            of.write(json.dumps(dump_data))
+        for chrm in self.reads:
+            dump_data[chrm] = []
+            for interval in self.reads[chrm]:
+                if interval.max_coverage >= k:
+                    dump_data[chrm].append({'start': interval.start,
+                                            'max_coverage': interval.max_coverage,
+                                            'size': interval.size})
+        with open(file_name, 'w') as output_f:
+            output_f.write(json.dumps(dump_data))
--- Tech Challenge/build_overlaps.py
+++ Tech Challenge/build_overlaps.py
@@ -8,50 +8,52 @@ from collections import deque
 from bed_interval import Interval, BedData
 start_t = datetime.now()
 
-def bring_in_data(bed_file):
-    file_data = {}
-    with open(bed_file) as infile:
-        for line in infile:
-            line = line.strip()
-            chrm, start, end, = line.split("\t")[0:3]
-            if chrm not in file_data:
-                file_data[chrm] = []
-            file_data[chrm].append((int(start), int(end)))
-    return file_data
+# def bring_in_data(bed_file):
+#     file_data = {}
+#     with open(bed_file) as infile:
+#         for line in infile:
+#             line = line.strip()
+#             chrm, start, end, = line.split("\t")[0:3]
+#             if chrm not in file_data:
+#                 file_data[chrm] = []
+#             file_data[chrm].append((int(start), int(end)))
+#     return file_data
+# def mark_coverage(bed,chrm,interval):
 
 
-def get_overlaps(chrm, f_name, data, overlap_amt):
-    #   compare starts and ends
-    for seg1 in bed_dict[f_name][chrm]:
-        seg1
-        start1, end1 = seg1
-        overlaps = ''
-        for file in data:
-            if file == f_name:
-                continue
-            for start2, end2 in data[file][chrm]:
-                start = max(start1,start2) #+1
-                end = min(end1,end2)
-                overlap = end - start
-                if overlap >= overlap_amt:
-                    overlaps+='\t'.join((chrm, str(start), str(end), f_name, file)) + '\n'
-    return overlaps
+# def get_overlaps(chrm, f_name, data, overlap_amt):
+#     #   compare starts and ends
+#     for seg1 in bed_dict[f_name][chrm]:
+#         seg1
+#         start1, end1 = seg1
+#         overlaps = ''
+#         for file in data:
+#             if file == f_name:
+#                 continue
+#             for start2, end2 in data[file][chrm]:
+#                 start = max(start1,start2) #+1
+#                 end = min(end1,end2)
+#                 overlap = end - start
+#                 if overlap >= overlap_amt:
+#                     overlaps+='\t'.join((chrm, str(start), str(end), f_name, file)) + '\n'
+#     return overlaps
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="""Make overlaps data""")
-    parser.add_argument("bed_files_path", type=str, default="", help="path to bed files to be read")
     parser.add_argument("overlap", help="Amount of overlap between fragments", type=int,
                         default=50)
     parser.add_argument("k", type=int, help="minimum number of other files a segment must"
                                             " overlap", default=1)
+    parser.add_argument("-path", type=str, default="",
+                        help="path to bed files to be read")
     parser.add_argument('--verbose', '-v', action='count', default=0,
                         help='Multiple flags increase verbosity')
     parser.add_argument('-test', '-t', action='store_true',
                         help='Do not store the data')
     args = parser.parse_args()
-    bed_files = glob.glob('{}*[0-9].bed'.format(args.bed_files_path))
+    bed_files = glob.glob('{}*[0-9].bed'.format(args.path))
     # in_files = deque([BedData() for f in range(bed_files.len())])
     # fs = args.bed_files.split(',')
     beds = []
@@ -62,28 +64,45 @@ if __name__ == "__main__":
     current_files = deque(beds)
     overlaps = BedData()
 
+    unmade_beds = []
     while current_files:
         my_bed = current_files.popleft()
-        for chrm in my_bed.reads
-            for i in my_bed.reads[chrm]:
-                my_bed.reads[chrm][i].start
+        for chrm in my_bed.reads:
+            read_count = len(my_bed.reads[chrm])
+            # get self overlaps in current file
+            for i in range(read_count):
+                i_stt = my_bed.reads[chrm][i].start
+                i_end = my_bed.reads[chrm][i].end
+                for j in range(i+1, read_count):
+                    j_stt = my_bed.reads[chrm][j].start
+                    if i_end - j_stt >= args.overlap:
+                        # print('Coverage occurred')
+                        my_bed.reads[chrm][i].add_coverage(j_stt,
+                                                           min(j_stt, i_end), 1)
+                for b in unmade_beds:
+                    other_bed_len = len(b.reads[chrm])
+                    for j in range(other_bed_len):
+                        j_stt = b.reads[chrm][j].start
+                        if i_end - j_stt >= args.overlap:
+                            # print('Coverage occurred')
+                            my_bed.reads[chrm][i].add_coverage(j_stt,
+                                                               min(j_stt, i_end), 1)
+                if my_bed.reads[chrm][i].max_coverage > 0:
+                    overlaps.copy_interval(chrm, my_bed.reads[chrm][i])
+        unmade_beds.append(my_bed)
 
-        overlaps.add_interval
-    # while True:
-    #     try:
-    #         bed_file = BedData()
-    #         f = in_files.popleft()
-    #         bed_file.load_bed(f)
-    #     except IndexError:
-    #         break
-    import json
-    result = json.dumps(o_laps)
-    f = open('overlaps.json', 'w')
-    f.write(result)
-    f.close()
+    overlaps_f_name = 'overlaps_of_{}-{}'.format(bed_files[0][-5], bed_files[-1][-5])
+    overlaps.dump_intervals(overlaps_f_name+'out.bed', args.k)
+    overlaps.dump_intervals_json(overlaps_f_name+'out.json', args.k)
 
-    with open('overlap.bed', 'w') as f2:
-        f2.write(o_laps)
-        f2.close()
+    # import json
+    # result = json.dumps(o_laps)
+    # f = open('overlaps.json', 'w')
+    # f.write(result)
+    # f.close()
+    #
+    # with open('overlap.bed', 'w') as f2:
+    #     f2.write(o_laps)
+    #     f2.close()
     print("loaded in " + str(datetime.now() - start_t))
 
